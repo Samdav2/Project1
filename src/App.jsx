6a56c5fc3ae022ec87839423c1a017ec47921c23
Stephen <chukwuebukastephen8@gmail.com>
2024-11-03 13:07:02 -0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {ResetPassword} from "./pages/ResetPassword"
+import {EventForm} from "./pages/TicketingForm/EventForm"
+import {NotificationPage} from "./pages/NotificationScreen/NotificationPage"
 
 
 import './App.css'
@@ -11,7 +13,9 @@ function App() {
   return (
     <Router>
       <Routes>
-         <Route path='/' element={<ResetPassword/>}/>
+         <Route path='/ResetPassword' element={<ResetPassword/>}/>
+         <Route path='/EventForm' element={<EventForm/>}/>
+         <Route path='/NotificationPage' element={<NotificationPage/>}/>
       </Routes>
     </Router>
     
