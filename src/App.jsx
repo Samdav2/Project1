9e3a99013f535a586f1c6c3c85de5d6862b5a561
Stephen <chukwuebukastephen8@gmail.com>
2024-11-03 10:50:25 -0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Sidebar from "./components/Ui/Sidebar";
-import MainDashboard from "./pages/MainDashboard/MainDashboard";
-import Header from './components/Ui/Header';
-import AnalyticsCards from './components/Ui/AnalyticsCards';
-import BigCards from './components/Ui/BigCards';
-import Statistics from './components/Ui/Statistics';
-import ProjectList from './components/Ui/ProjectList';
-import OrdersOverview from './components/Ui/OrdersOverview';
-import Footer from './components/Ui/Footer';
-import './App.css';
+import {ResetPassword} from "./pages/ResetPassword"
+
+
+import './App.css'
 
 
 
 function App() {
   return (
-    <div className="app">
-      <Sidebar />
-      <MainDashboard />
-      <Header />
-      <OrdersOverview />
-      <AnalyticsCards />
-      <BigCards />
-      <Statistics />
-      <ProjectList />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+         <Route path='/' element={<ResetPassword/>}/>
+      </Routes>
+    </Router>
     
-  );
+    
+  )
 }
 
 
-export default App;
+export default App
