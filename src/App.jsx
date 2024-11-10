5a83025ce8609605896bcf4c75e7365245c07c52
Stephen <chukwuebukastephen8@gmail.com>
2024-11-09 22:18:58 -0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import {ResetPassword} from "./pages/ResetPassword"
+/*import { BrowserRouter as Router, Route, Routes } from "react-router-dom";*/
+/*import {ResetPassword} from "./pages/ResetPassword"*/
+/*import {TicketingForm} from "./pages/TicketingForm/TicketingForm"*/
+/*import {RegistrationForm} from "./pages/TablesRegistrationForm/RegistrationForm"*/
+import {EventPage} from "./pages/PrivateEvent/EventPage"
 
 
 import './App.css'
@@ -9,14 +12,24 @@ import './App.css'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-         <Route path='/' element={<ResetPassword/>}/>
-      </Routes>
-    </Router>
+
+    /*<ResetPassword />*/
+
+    /*<TicketingForm />*/
+
+    /*<RegistrationForm />*/
+
+    <EventPage />
+
+    /*<Router>
+    <Routes>
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/Ticketing-form" element={<TicketingForm />} />
+      {/* Add more routes as needed */
+    /*</Routes>*/
+  /*</Router>}*/
     
-    
-  )
+  );
 }
 
 export default App;
