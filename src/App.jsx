dc5a256769f70b132fa5ed79ed8476257c4bcf07
Amiola Demilade <amiolademilade@gmail.com>
2024-11-22 16:33:00 +0100

    New Changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ import {EventPage} from "./pages/PrivateEvent/EventPage"
 /*import {NotificationPage} from "./pages/NotificationScreen/NotificationPage"*/
 import {EmptyNotification} from "./pages/NotificationScreen/EmptyNotification"
 import {CardFormCard} from "./pages/PaymentPage/CardFormCard"
-
-
+import SignUpPage from "./pages/SignUp/signUp";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import EmailVerify from "./components/Ui/emailVerify";
+import ProfileCreation from "./pages/ProfileCreation/ProfileCreation";
+import MainDashboard from "./pages/MainDashboard/MainDashboard";
+import Sidebar from "./components/Ui/Sidebar"
 import './App.css'
 
 
@@ -18,12 +22,19 @@ function App() {
 
       <div className="main-content">
         <Routes>
-          <Route path="/" element={<MainDashboard />} />
+         {/* <Route path="/" element={<MainDashboard />} /> */}
          {/* <Route path="/analytics" element={<AnalyticsCards />} />
           <Route path="/big-cards" element={<BigCards />} />
           <Route path="/statistics" element={<Statistics />} />
           <Route path="/projects" element={<ProjectList />} />
           <Route path="/orders" element={<OrdersOverview />} /> */}
+          <Route path="/" element={ <SignUpPage />} />
+          <Route path="/login" element={ <LoginPage />} />
+          <Route path="/email" element={<EmailVerify/>} />
+          <Route path="/create-profile" element={<ProfileCreation />} />
+          <Route path="/creator-dashboard" element={<MainDashboard />} />
+
+
         </Routes>
       </div>
 
