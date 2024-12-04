a15752d9233f8fee178baf336d839cb77d73addd
Stephen <chukwuebukastephen8@gmail.com>
2024-12-04 14:18:15 -0800

    changes on dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,25 +1,51 @@
-import React from "react";
-import Navbar from "../NavBar";
+import React, { useState } from "react";
 import Overview from "./Overview";
-import EventsList from "./EventsList";
 import Analytics from "./Analytics";
+import EventsList from "./EventsList";
 import ProfileSettings from "./ProfileSettings";
-import Footer from "./Footer";
-import "./DashBoard.css";
+import Footer from "./Footer"
+import "./Dashboard.css";
 
-function DashBoard() {
+const Dashboard = () => {
+  const [organizer, setOrganizer] = useState({
+    name: "Organizer Name",
+    email: "organizer@example.com",
+  });
+
   return (
     <div className="dashboard">
-      <Navbar />
+        <div className="profile-section">
+          <img
+            src={organizer.profilePicture}
+            alt={`${organizer.name}'s Profile`}
+            className="profile-picture"
+          />
+          <div className="organizer-details">
+            <h1>Welcome, {organizer.name}</h1>
+            <p>Gmail: {organizer.email}</p>
+            <p>Last Login: {organizer.lastLogin}</p>
+          </div>
+        </div>
+        
       <div className="dashboard-content">
         <Overview />
-        <EventsList />
+      </div>
+
+      <div className="dashboard-actions">
+        <button className="action-button create-event">Create Event</button>
+        <button className="action-button1 verify-ticket">Verify Ticket</button>
+      </div>
+     
+      <div className="dashboard-content">
+        <Overview />
         <Analytics />
+        <EventsList />
         <ProfileSettings />
         <Footer />
       </div>
+
     </div>
   );
-}
+};
 
-export default DashBoard;
+export default Dashboard;
